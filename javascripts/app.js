a6dcd2931b3e57ae9166fbb626b2150d7c317aed
Sidney Burks <sid137@gmail.com>
2012-11-25 23:42:57 +0100

    lighting fast

--- javascripts/app.js
+++ javascripts/app.js
@@ -8,7 +8,9 @@ App.config(function($interpolateProvider) {
 
 App.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   $locationProvider.html5Mode(true);
+  $routeProvider.when('/', {templateUrl: 'partials/index.html'});
   $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
+  $routeProvider.when('/contact', {templateUrl: 'partials/contact.html'});
   $routeProvider.otherwise({redirectTo: '/'});
 
 }]);
