fddadcd13017a365168cd8d0bbb87b8d4f57e0e3
Sidney Burks <sid137@gmail.com>
2012-11-26 02:56:56 +0100

    basic angular scaffold up

--- _site/javascripts/app.js
+++ _site/javascripts/app.js
@@ -11,6 +11,33 @@ App.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
   $routeProvider.when('/', {templateUrl: 'partials/index.html'});
   $routeProvider.when('/about', {templateUrl: 'partials/about.html'});
   $routeProvider.when('/contact', {templateUrl: 'partials/contact.html'});
+  $routeProvider.when('/projects/:project', {templateUrl: '../partials/project_detail.html', controller: "ProjectDetailCtrl"});
   $routeProvider.otherwise({redirectTo: '/'});
 
 }]);
+
+App.run(function($rootScope){
+  $rootScope.projects = [
+    {
+      thumbnail: "img/atoms_mot.small.jpg",
+      alt: "Magneto-optical trap containing atomic storage medium",
+      page: "/projects/quantum_memory",
+      title: "Quantum Memory for Squeezed Light",
+      description: "Developing the building blocks for the computers of tomorrow.  Storing information onto atoms using light"
+    },
+    {
+      thumbnail: "img/flowers-lg.jpg",
+      alt: "Transcription of phone call audio",
+      page: "/projects/roger",
+      title: "\"Roger\" Speech Transcription",
+      description: "An application to improve the quality of life of the hearing-impaired, by providing easier communications mechanism."
+    },
+    {
+      thumbnail: "img/leaves-lg.jpg",
+      alt: "BillFloat.com web site",
+      page: "/projects/ruby_on_rails",
+      title: "Ruby on Rails",
+      description: "Front and backend web development for Billfloat.com, using Ruby on Rails"
+    }
+  ];
+});
--- _site/javascripts/controllers.js
+++ _site/javascripts/controllers.js
@@ -1,26 +1,8 @@
-function AppCtrl($scope){
-  $scope.test = "Hello from AppCtrl!";
-  $scope.projects = [
-    {
-      thumbnail: "img/atoms_mot.small.jpg",
-      alt: "Magneto-optical trap containing atomic storage medium",
-      page: "portfolio-single.html",
-      title: "Quantum Memory for Squeezed Light",
-      description: "Developing the building blocks for the computers of tomorrow.  Storing information onto atoms using light"
-    },
-    {
-      thumbnail: "img/flowers-lg.jpg",
-      alt: "Transcription of phone call audio",
-      page: "portfolio-single.html",
-      title: "\"Roger\" Speech Transcription",
-      description: "An application to improve the quality of life of the hearing-impaired, by providing easier communications mechanism."
-    },
-    {
-      thumbnail: "img/leaves-lg.jpg",
-      alt: "BillFloat.com web site",
-      page: "portfolio-single.html",
-      title: "Ruby on Rails",
-      description: "Front and backend web development for Billfloat.com, using Ruby on Rails"
-    }
-  ];
+function AppCtrl($scope, $routeParams, $http){
+}
+
+function ProjectDetailCtrl($scope, $routeParams, $http){
+  $http.get('/javascripts/' + $routeParams.project + '.json').success(function(data){
+    $scope.project = data;
+  });
 }
