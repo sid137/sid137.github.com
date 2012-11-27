6e28639d18295403d18854776ac68fea7777f119
Sidney Burks <sid137@gmail.com>
2012-11-27 05:20:50 +0100

    adding things

--- _site/javascripts/app.js
+++ _site/javascripts/app.js
@@ -1,6 +1,19 @@
 
 var App = angular.module('App', []);
 
+App.links = {
+  blog_url: "http://www.sid137.com", 
+  twitter_url: "https://twitter.com/sid137",
+  linked_in_url: "http://www.linkedin.com/in/sid137",
+  google_plus_url: "http://plus.google.com/105375603751571213131/about",
+  github_url: "https://github.com/sid137",
+  stack_overflow_url: "http://stackoverflow.com/users/262972/noli",
+  root_url: "/", 
+  about_url: "/about",
+  contact_url: "/contact",
+  projects_url: "/projects/:project"
+};
+
 App.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
@@ -8,11 +21,11 @@ App.config(function($interpolateProvider) {
 
 App.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
   $locationProvider.html5Mode(true);
-  $routeProvider.when('/', {templateUrl: 'main.html'});
-  $routeProvider.when('/about.html', {templateUrl: 'about.html'});
-  $routeProvider.when('/contact.html', {templateUrl: 'contact.html'});
-  $routeProvider.when('/projects/:project', {templateUrl: 'project_detail.html', controller: "ProjectDetailCtrl"});
-  $routeProvider.otherwise({redirectTo: '/'});
+  $routeProvider.when(App.links.root_url, {templateUrl: 'main.html'});
+  $routeProvider.when(App.links.about_url, {templateUrl: 'about.html'});
+  $routeProvider.when(App.links.contact_url, {templateUrl: 'contact.html'});
+  $routeProvider.when(App.links.projects_url, {templateUrl: 'project_detail.html', controller: "ProjectDetailCtrl"});
+  $routeProvider.otherwise({redirectTo: App.links.root_url});
 
 }]);
 
@@ -24,6 +37,7 @@ App.run(function($rootScope){
       thumbnail: "img/atoms_mot.small.jpg",
       alt: "Magneto-optical trap containing atomic storage medium",
       page: "/projects/quantum_memory",
+      name: "quantum_memory",
       title: "Quantum Memory for Squeezed Light",
       description: "Developing the building blocks for the computers of tomorrow.  Storing information onto atoms using light"
     },
@@ -31,6 +45,7 @@ App.run(function($rootScope){
       thumbnail: "img/flowers-lg.jpg",
       alt: "Transcription of phone call audio",
       page: "/projects/roger",
+      name: "roger", 
       title: "\"Roger\" Speech Transcription",
       description: "An application to improve the quality of life of the hearing-impaired, by providing easier communications mechanism."
     },
@@ -38,17 +53,12 @@ App.run(function($rootScope){
       thumbnail: "img/leaves-lg.jpg",
       alt: "BillFloat.com web site",
       page: "/projects/ruby_on_rails",
+      name: "ruby_on_rails",
       title: "Ruby on Rails",
       description: "Front and backend web development for Billfloat.com, using Ruby on Rails"
     }
   ];
 
-  $rootScope.links = {
-    twitter_url: "https://twitter.com/sid137",
-    linked_in_url: "http://www.linkedin.com/in/sid137",
-    google_plus_url: "http://plus.google.com/105375603751571213131/about",
-    github_url: "https://github.com/sid137",
-    stack_overflow_url: "http://stackoverflow.com/users/262972/noli"
-  };
+  $rootScope.links = App.links;
 
 });
